ee84077ec16a0d9c2f91282b302d56a40eb93285
SAI <sai552kk@gmail.com>
2024-05-20 15:19:22 +0530

    Adding default return next() in the middleware file

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,6 +17,7 @@ export async function middleware(request: NextRequest) {
   ) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
+  return NextResponse.next()
 }
 
 // See "Matching Paths" below to learn more
