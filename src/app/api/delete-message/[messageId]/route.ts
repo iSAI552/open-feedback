5fd1ab20d6c9a8fcccc09529ef22e85aa8819ed4
SAI <sai552kk@gmail.com>
2024-05-21 18:53:33 +0530

    Making delete message dynamic route

---
+++ src/app/api/delete-message/[messageId]/route.ts
@@ -0,0 +1,70 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/user.models";
+import { User } from "next-auth";
+
+export async function DELETE(request: Request, {params}: {params: {messageId: string}}) {
+  await dbConnect();
+    const messageId = params.messageId
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user as User;
+
+  if (!session || session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not Authenticated",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  try {
+    const updatedResult = await UserModel.updateOne(
+        {_id: user._id},
+        {$pull: {
+            messages: {
+                _id: messageId
+            }
+        }}
+    )
+
+    if(updatedResult.modifiedCount === 0){
+        return Response.json(
+            {
+              success: false,
+              message: "Message not found or already deleted",
+            },
+            {
+              status: 404,
+            }
+          );
+    }
+
+    return Response.json(
+        {
+          success: true,
+          message: "Message Deleted",
+        },
+        {
+          status: 201,
+        }
+      );
+
+  } catch (error) {
+    console.error("Error in delete message route", error)
+    return Response.json(
+        {
+          success: false,
+          message: "Error deleting the message",
+        },
+        {
+          status: 500,
+        }
+      );
+  }
+
+}
