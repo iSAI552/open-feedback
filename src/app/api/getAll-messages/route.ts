5cfef24bed71c3b41db49a406152c76188178de9
SAI <sai552kk@gmail.com>
2024-05-26 13:53:28 +0530

    making a get-all messages api

--- src/app/api/getAll-messages/route.ts
+++ src/app/api/getAll-messages/route.ts
@@ -2,54 +2,74 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/options";
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/user.models";
-import { User } from "next-auth";
-import mongoose from "mongoose";
 
 export async function GET(request: Request) {
-    await dbConnect();
-  
-    const session = await getServerSession(authOptions);
-    const user: User = session?.user as User;
-  
-    // if (!session || !session.user) {
-    //   return Response.json(
-    //     {
-    //       success: false,
-    //       message: "Not Authenticated",
-    //     },
-    //     {
-    //       status: 401,
-    //     }
-    //   );
-    // }
+  await dbConnect();
 
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not Authenticated",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  try {
     const allMessages = await UserModel.aggregate([
-        {
-            $unwind: "$messages"
+      {
+        $unwind: "$messages",
+      },
+      {
+        $sort: {
+          "messages.createdAt": -1,
         },
-        {
-            $sort: {
-                "messages.createdAt": -1
-            }
+      },
+      {
+        $project: {
+          username: 1,
+          message: "$messages.content",
         },
-        {
-            $project: {
-                username: 1,
-                message: "$messages"
-            }
-        }
-    ]).exec()
+      },
+    ]).exec();
 
-    console.log(allMessages)
+    if (!allMessages) {
+      console.log("Error while getting all the messages");
+      return Response.json(
+        {
+          success: false,
+          message: "An unexpected occur has occured",
+        },
+        {
+          status: 500,
+        }
+      );
+    }
 
     return Response.json(
-        {
-            success: true,
-            messages: allMessages
-        },
-        {
-            status: 200
-        }
-    )
-
+      {
+        success: true,
+        messages: allMessages,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.log("An unexpected occur has occured while getting All messages", error);
+    return Response.json(
+      {
+        success: false,
+        message: "An unexpected occur has occured while getting all the messages",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
