6e15cbd9797deebef1c8b195db12cbd5b8c5f30e
SAI <sai552kk@gmail.com>
2024-05-28 14:33:52 +0530

    Added the Dialog box to directly send to the page by giving username

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,8 +3,57 @@ import Link from "next/link"
 import { useSession, signOut } from "next-auth/react"
 import { User } from "next-auth"
 import { Button } from "./ui/button"
-import { ContactRoundIcon } from "lucide-react"
+import { ContactRoundIcon, Forward, MailIcon } from "lucide-react"
 import { useRouter } from "next/navigation"
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Label } from "./ui/label"
+import { Input } from "./ui/input"
+import { useState } from "react"
+
+function DialogBox() {
+    const [username, setUsername] = useState('')
+    return (
+        <div>
+            <Dialog>
+                <DialogTrigger asChild >
+                    <Button variant="ghost"><MailIcon className="w-full md:w-auto"/></Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-md">
+                    <DialogHeader>
+                        <DialogTitle>Anonymous message</DialogTitle>
+                        <DialogDescription>
+                            Share an anonymous message with:
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className="flex items-center space-x-2">
+                        <div className="grid flex-1 gap-2">
+                            <Label htmlFor="username" className="sr-only">
+                                Link
+                            </Label>
+                            <Input
+                                id="username"
+                                placeholder="Username"
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                        </div>
+                        <Link href={`u/${username}`}>
+                        <span className="sr-only">Go</span>
+                            <Forward className="h-4 w-4" />
+                        </Link>
+                    </div>
+                </DialogContent>
+            </Dialog>
+        </div>
+    )
+}
 
 export default function Navbar() {
 
@@ -21,15 +70,19 @@ export default function Navbar() {
                     session ? (
                         <>
                             <span className="mr-4">Welcome, {user?.username || user?.email}</span>
-                            <div className="flex ">
-                            <Button className="w-full md:w-auto mr-4" onClick={() => router.replace('/dashboard')}><ContactRoundIcon/></Button>
-                            <Button className="w-full md:w-auto" onClick={() => signOut()}>Logout</Button>
+                            <div className="flex">
+                                <DialogBox />
+                                <Button variant="ghost" className="w-full md:w-auto mr-4 ml-4" onClick={() => router.replace('/dashboard')}><ContactRoundIcon /></Button>
+                                <Button variant="outline" className="w-full md:w-auto" onClick={() => signOut()}>Logout</Button>
                             </div>
                         </>
                     ) : (
-                        <Link href='/sign-in'>
+                        <div className="flex">
+                        <DialogBox />
+                        <Link className="ml-4" href='/sign-in'>
                             <Button className="w-full md:w-auto">Login</Button>
                         </Link>
+                        </div>
                     )
                 }
             </div>
