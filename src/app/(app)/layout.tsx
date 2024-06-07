77824ff425d6a12b5d456fd3f8a7b3f7b4f461b0
SAI <sai552kk@gmail.com>
2024-06-07 14:42:05 +0530

    Refactor layout component to include a flex container for improved styling

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -17,8 +17,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <div className="flex flex-col min-h-screen">
       <Navbar />
         {children}
+        </div>
       </body>
     </html>
   );
