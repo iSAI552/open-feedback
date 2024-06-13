2069f8ad30f79878f229a5061197793beda00dfe
SAI <sai552kk@gmail.com>
2024-06-13 13:28:59 +0530

    Adding the better Ui for the verify email page

--- src/app/(auth)/verify/[username]/page.tsx
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { Button } from "@/components/ui/button"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage, FormDescription } from "@/components/ui/form"
 import { useToast } from "@/components/ui/use-toast"
 import { verifyCodeSchema } from "@/schemas/verifyCode.schema"
 import { ApiResponse } from "@/types/apiResponse"
@@ -11,6 +11,15 @@ import { Input } from "@/components/ui/input"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { useState } from "react"
+import { REGEXP_ONLY_DIGITS, REGEXP_ONLY_DIGITS_AND_CHARS } from "input-otp"
+
+import {
+    InputOTP,
+    InputOTPGroup,
+    InputOTPSeparator,
+    InputOTPSlot,
+  } from "@/components/ui/input-otp"
+  
 
 export default function VerifyAccount() {
     const router = useRouter()
@@ -49,38 +58,43 @@ export default function VerifyAccount() {
             setLoading(false)
         }
     }
-
     return (
         <div className="flex justify-center items-center min-h-screen bg-gray-800">
-            <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
-                <div className="text-center">
-                    <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
-                        Verify Your Account
-                    </h1>
-                    <p className="mb-4">Enter the verification code sent your email</p>
-                </div>
-                <Form {...form}>
+       <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-          <FormField
-          name="code"
-          control={form.control}
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Verification Code</FormLabel>
-              <FormControl>
-                <Input  placeholder="code" {...field} />
-              </FormControl>
-              {/* <FormDescription>
-                This is your email
-              </FormDescription> */}
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">{loading ? "Loading": "Verify"}</Button>
+            
+            <FormField
+              control={form.control}
+              name="code"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Verify Your Email</FormLabel>
+                  <FormControl>
+                    <InputOTP pattern={REGEXP_ONLY_DIGITS} maxLength={6} {...field}>
+                      <InputOTPGroup>
+                        <InputOTPSlot index={0} />
+                        <InputOTPSlot index={1} />
+                        <InputOTPSlot index={2} />
+                        <InputOTPSeparator />
+                        <InputOTPSlot index={3} />
+                        <InputOTPSlot index={4} />
+                        <InputOTPSlot index={5} />
+                      </InputOTPGroup>
+                    </InputOTP>
+                  </FormControl>
+                  <FormDescription>
+                    Please enter the verification code sent to your Email.
+                  </FormDescription>
+                  {/* <FormMessage /> */}
+                </FormItem>
+              )}
+            />
+     
+            <Button className="rounded-sm bg-gray-900" type="submit">Submit</Button>
           </form>
         </Form>
-            </div>
         </div>
-    )
+        </div>
+      )
 }
